715a874f0e67885d02d3346e59904418a8790088
Himani Goel <91362426+HimaniGoel4@users.noreply.github.com>
2025-01-03 01:44:54 +0530

    Create article.js

---
+++ backend/models/article.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const ArticleSchema = new mongoose.Schema({
+    title: { type: String, required: true },
+    author: { type: String },
+    publishedAt: { type: Date },
+    content: { type: String },
+    source: { type: String },
+    processed: { type: Boolean, default: true }
+});
+
+module.exports = mongoose.model('Article', ArticleSchema);
